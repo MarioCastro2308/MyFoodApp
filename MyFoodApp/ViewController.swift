31b95a56cffa4b7b30e8c00df9cab6f96192184f
Mario Castro <mario.castro.2308@outlook.com>
2023-06-15 12:15:03 -0600

    Initial Commit

---
+++ MyFoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFoodApp
+//
+//  Created by Mario Castro on 15/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
